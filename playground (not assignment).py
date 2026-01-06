9fef98e953a7d3ddd6e1660ca32c96387c2b9aca
Jude Martin <jm129303@student.wpsok.org>
2026-01-06 12:46:11 -0600

    TEST

--- playground (not assignment).py
+++ playground (not assignment).py
@@ -1,15 +1,52 @@
 import random
-import time
-a = ["Bar", "7", "Cherry", "Banana", "Sam"]
-b = ["Bar", "7", "Cherry", "Banana", "Sam"]
-c = ["Bar", "7", "Cherry", "Banana", "Sam"]
-random.shuffle(a)
-random.shuffle(b)
-random.shuffle(c)
-d = a[0]
-e = b[0]
-f = c[0]
-slotmachine = d, e, f
-print(slotmachine)
-if d == "Bar" and e == "Bar" and f == "Bar":
-    print(d)
+from base64 import a85decode
+
+cur = random.randint(2, 13)
+nex = random.randint(2, 13)
+'''
+while True:
+    c = input(f"{cur} is your number. Will the next be higher or lower?   ")
+    if nex == cur:
+        nex = random.randint(1, 12)
+    if c == "higher" and cur < nex:
+        print(f"Correct! {nex} was higher than {cur}.")
+        cur = random.randint(1, 12)
+        cur, nex = nex, cur
+    elif c == "lower" and cur > nex:
+        print(f"Correct! {nex} was lower than {cur}.")
+        cur = random.randint(1, 12)
+        cur, nex = nex, cur
+    elif c == "lower" and cur < nex:
+        print(f"Wrong. {nex} was higher than {cur}.")
+        d = input("Do you want to continue? yes/no  ")
+        if d == "yes":
+            continue
+        else:
+            break
+    elif c == "higher" and cur > nex:
+        print(f"Wrong. {nex} was lower than {cur}.")
+        d = input("Do you want to continue? yes/no  ")
+        if d == "yes":
+            continue
+        else:
+            break
+    else:
+        print("CHOOSE ONLY WHAT I TOLD YOU TO CHOOSE")
+        d = input("Do you want to continue? yes/no  ")
+        if d == "yes":
+            continue
+        else:
+            break
+            '''
+ezlist = ["rock", "paper", "scissors"]
+random.shuffle(ezlist)
+
+print("rock paper scissors")
+
+bleh = input("do you want to play against easy or hard npc?")
+if bleh == "easy":
+    while True:
+        input("choose rock, paper, or scissors  ")
+        random.shuffle(ezlist)
+        print(ezlist[0])
+        if bleh == "rock" :
