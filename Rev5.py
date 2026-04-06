bc0e1188d902c1268b7162ced160acc8b0f4cc9f
Aiden Funkhouser <af128219@student.wpsok.org>
2026-04-06 12:54:57 -0500

    Rev5

---
+++ Rev5.py
@@ -0,0 +1,14 @@
+#Name: Aiden Funkhouser
+#Class: 5th Hour
+#Assignment: HW-R5
+
+#1. Create a list of the names of all the students in the classroom.
+classroom = ["Dylan", "Ivan", "Brenlyn", "Sam", "Aiden", "Bryson", "Ashton", "Hogan"]
+#2. Create a for loop that prints the names of every student in the list.
+for x in classroom:
+    print(x)
+#3. Using the "in" operator (hint: Google), create a for loop that only prints
+#the name of a student if the letter "e" is in it.
+for c in classroom:
+    if "e" in c:
+        print(c)
